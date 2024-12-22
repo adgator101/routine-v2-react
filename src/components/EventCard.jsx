c3e04612f209651379e97000c35265a80bb97b12
Aaditya <aadityathapa977@gmail.com>
2024-12-22 20:27:20 +0545

    feat(event-card): add new event card component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import { MapPin } from "lucide-react";
+import React from "react";
 const EventCard = () => {
   return (
-    <div>EventCard</div>
-  )
-}
+    <div className="flex w-4/12 flex-col gap-6 rounded-lg bg-gray-200 px-3 py-4 shadow-md">
+      <div className="event-card-top flex items-center gap-10">
+        <div className="event-card-image">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2299/2299172.png"
+            alt=""
+            width={100}
+          />
+        </div>
+        <div className="event-type text-2xl">Cyber Security 101</div>
+      </div>
+      <div className="event-card-bottom flex items-center gap-10">
+        <div className="event-card-countdown flex gap-1 text-[#505070]">
+          <div className="event-card-countdown-time text-4xl font-extrabold">
+            16D
+          </div>
+          <div className="event-card-countdown-extra-info self-end text-sm">
+            left
+          </div>
+        </div>
+        <div className="event-location flex items-center gap-1">
+          <MapPin fill="gray" />
+          <p>Biratnagar International College</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EventCard
+export default EventCard;
